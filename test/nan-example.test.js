7513366bf5a451b16f1559f00d7d3e5c8ba35be6
fengmk2 <fengmk2@gmail.com>
2014-05-05 12:49:15 +0800

    first commit

---
+++ test/nan-example.test.js
@@ -0,0 +1,22 @@
+/**!
+ * nan-example - test/nan-example.test.js
+ *
+ * Copyright(c) 2014 fengmk2 and other contributors.
+ * MIT Licensed
+ *
+ * Authors:
+ *   fengmk2 <fengmk2@gmail.com> (http://fengmk2.github.com)
+ */
+
+"use strict";
+
+/**
+ * Module dependencies.
+ */
+
+var should = require('should');
+var nan-example = require('../');
+
+describe('nan-example.test.js', function () {
+
+});
